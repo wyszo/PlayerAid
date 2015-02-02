80243b701ad2c4b6be7d9c68a982e7dcbcd34136
Wyszo <wyszo@Tomeks-MacBook-Pro.local>
2015-02-02 19:14:50 +0100

    Latest & following filter buttons added

--- PlayerAid/HomeViewController.m
+++ PlayerAid/HomeViewController.m
@@ -4,15 +4,32 @@
 
 #import "HomeViewController.h"
 
+
 @interface HomeViewController ()
 
+@property (weak, nonatomic) IBOutlet UIView *latestFilterView;
+@property (weak, nonatomic) IBOutlet UIView *followingFilterView;
+
 @end
+
 
 @implementation HomeViewController
 
 - (void)viewDidLoad {
   [super viewDidLoad];
   self.title = @"Home";
+}
+
+#pragma mark - latest & following buttons bar
+
+- (IBAction)latestFilterSelected:(id)sender
+{
+  // TODO: change predicate to show latest tutorials
+}
+
+- (IBAction)followingFilterSelected:(id)sender
+{
+  // TODO: change predicate to show tutorials of the users I follow
 }
 
 @end
