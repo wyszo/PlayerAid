bbbbee55cd7e733f434c67e2583a7447535fa0af
Wyszo <wyszo@Tomeks-MacBook-Pro.local>
2015-02-02 03:01:56 +0100

    Capturing Facebook email address. If a user doesn't have a registered email, using username@facebook.com

--- PlayerAid/IntroViewController.m
+++ PlayerAid/IntroViewController.m
@@ -3,28 +3,57 @@
 //
 
 #import <FacebookSDK/FacebookSDK.h>
+#import <FacebookSDK/FBGraphUser.h>
 #import "IntroViewController.h"
 
-@interface IntroViewController ()
-
+@interface IntroViewController () <FBLoginViewDelegate>
+@property (nonatomic, copy) NSString *userEmail;
 @end
 
 @implementation IntroViewController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    [self addFacebookLoginButton];
+  [super viewDidLoad];
+  [self addFacebookLoginButton];
 }
 
 #pragma mark - Facebook login
 
 - (void)addFacebookLoginButton{
-    FBLoginView *loginView = [[FBLoginView alloc] initWithReadPermissions:@[@"email"]];
-    loginView.center = self.view.center;
-    [self.view addSubview:loginView];
+  FBLoginView *loginView = [[FBLoginView alloc] initWithReadPermissions:@[@"email"]];
+  loginView.center = self.view.center;
+  loginView.delegate = self;
+  [self.view addSubview:loginView];
 }
 
-/*
+#pragma mark - FBLoginViewDelegate
+
+- (void)loginViewShowingLoggedInUser:(FBLoginView *)loginView {
+  // TODO: Now we can send the Facebook access token to our API (with user email address) and push the new view
+}
+
+// callback invoked before loginViewShowingLoggedInUser
+- (void)loginViewFetchedUserInfo:(FBLoginView *)loginView
+                            user:(id<FBGraphUser>)user {
+  
+  self.userEmail = [self emailFromUser:user];
+}
+
+- (NSString *)emailFromUser:(id<FBGraphUser>)user {
+  // it is possible to have a user without registered email address, but we always want to have one
+  NSString *email = [user objectForKey:@"email"];
+  return (email ? email : [NSString stringWithFormat:@"%@@facebook.com", user.username]);
+}
+
+- (void)loginViewShowingLoggedOutUser:(FBLoginView *)loginView {
+  
+}
+
+- (void)loginView:(FBLoginView *)loginView
+      handleError:(NSError *)error {
+  // TODO: show an alert with error perhaps?
+}
+
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -32,6 +61,5 @@
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
 }
-*/
 
 @end
