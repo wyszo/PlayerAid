dc8181165c0b0f23b5bfdcda00a3d9434592b4ff
Wyszo <wyszo@Tomeks-MacBook-Pro.local>
2015-02-04 21:39:51 +0100

    Tabbar buttons text color customised, background color and color of create item changed

--- PlayerAid/AppDelegate.m
+++ PlayerAid/AppDelegate.m
@@ -4,6 +4,7 @@
 
 #import <FacebookSDK/FacebookSDK.h>
 #import <MagicalRecord+Setup.h>
+#import <KZAsserts.h>
 #import "AppDelegate.h"
 #import "DataModelMock.h"
 
@@ -44,6 +45,35 @@
   // White fonts in statusbar and Navigation bars
   [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent];
   [[UINavigationBar appearance] setTitleTextAttributes:@{ NSForegroundColorAttributeName : [UIColor whiteColor] }];
+  
+  // Tabbar customisation
+  [[UITabBarItem appearance] setTitleTextAttributes:@{ NSForegroundColorAttributeName : [UIColor greenColor] }  forState:UIControlStateNormal];
+  [[UITabBarItem appearance] setTitleTextAttributes:@{ NSForegroundColorAttributeName : [UIColor redColor] } forState:UIControlStateSelected];
+  
+  // TabBar background color
+  [[UITabBar appearance] setBarTintColor:[UIColor yellowColor]];
+  
+  [self customiseCreateTabBarButtonBackground];
+ 
+  // TODO: change TabBar border line color
+}
+
+- (void)customiseCreateTabBarButtonBackground
+{
+  AssertTrueOrReturn([self.window.rootViewController isKindOfClass:[UITabBarController class]]);
+  UITabBarController *tabBarController = (UITabBarController *)self.window.rootViewController;
+  
+  const NSUInteger createItemIndex = 2;
+  AssertTrueOrReturn(tabBarController.tabBar.items.count > createItemIndex);
+  
+  UIColor *createButtonBackgroundColor = [UIColor colorWithRed:1.00 green:0.07 blue:0.7 alpha:1.0];
+  
+  UITabBar *tabBar = tabBarController.tabBar;
+  CGFloat tabbarItemWidth = tabBar.frame.size.width / (CGFloat)tabBar.items.count;
+  UIView *createButtonBackgroundView = [[UIView alloc] initWithFrame:CGRectMake(tabbarItemWidth * createItemIndex, 0, tabbarItemWidth, tabBar.frame.size.height)];
+  createButtonBackgroundView.backgroundColor = createButtonBackgroundColor;
+  
+  [tabBar insertSubview:createButtonBackgroundView atIndex:0];
 }
 
 - (void)populateCoreDataWithSampleEntities
