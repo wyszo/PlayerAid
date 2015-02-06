98bed1212602a1db06c16ad58fbf4484f2982443
Wyszo <wyszo@Tomeks-MacBook-Pro.local>
2015-02-06 12:18:46 +0100

    CreateTutorial NavigationBar buttons added

--- PlayerAid/CreateTutorialViewController.h
+++ PlayerAid/CreateTutorialViewController.h
@@ -4,6 +4,7 @@
 
 #import <UIKit/UIKit.h>
 
+
 @interface CreateTutorialViewController : UIViewController
 
 @end
---
+++ PlayerAid/NavigationBarCustomizationHelper.h
@@ -0,0 +1,12 @@
+//
+//  PlayerAid
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface NavigationBarCustomizationHelper : NSObject
+
++ (UIView *)containerViewhWithButtonWithFrame:(CGRect)frame title:(NSString *)buttonTitle target:(id)target action:(SEL)action;
+
+@end
