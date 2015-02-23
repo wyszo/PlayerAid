503cfde428dcbaaacbde1995373ab0d87773b9d8
Tomasz Wyszomirski <thomas@wyszomirski.co.uk>
2015-02-23 17:55:01 +0100

    QA and Staging environments added to the app (commented out for now)

--- PlayerAid/GlobalSettings.m
+++ PlayerAid/GlobalSettings.m
@@ -2,8 +2,15 @@
 //  PlayerAid
 //
 
+
 // Production Server
 //NSString const* kServerBaseURL = @"http://api.playeraid.co.uk/v1/";
 
-// Mock Server
+// QA Server
+//NSString const* kServerBaseURL = @"qa.api.playeraid.co.uk";
+
+// Staging environment
+//NSString const* kServerBaseURL = @"staging.api.playeraid.co.uk";
+
+// My Internal Mock Server
 NSString const* kServerBaseURL = @"http://demo6914797.mockable.io/";
