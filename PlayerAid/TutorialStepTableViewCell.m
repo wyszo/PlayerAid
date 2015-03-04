9159a00f7227c9189d6567fdb150e7d405d01fce
Tomasz Wyszomirski <thomas@wyszomirski.co.uk>
2015-03-04 14:05:43 +0100

    Image view handling added to Create Tutorial

--- PlayerAid/TutorialStepTableViewCell.m
+++ PlayerAid/TutorialStepTableViewCell.m
@@ -5,9 +5,16 @@
 #import "TutorialStepTableViewCell.h"
 
 
+static const CGFloat kContentImageHeight = 270.0f;
+
+
 @interface TutorialStepTableViewCell ()
 @property (weak, nonatomic) IBOutlet UITextView *textView;
-@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UIImageView *contentImageView;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *contentImageHeightConstraint;
+
+// obsolete
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *imageViewAspectRatioConstraint;
 @end
 
 
@@ -16,9 +23,37 @@
 - (void)configureWithTutorialStep:(TutorialStep *)tutorialStep
 {
   self.textView.text = tutorialStep.text;
-  self.imageView.image = tutorialStep.image;
+  [self updateImageViewWithImage:tutorialStep.image];
   
   // TODO: configure video tutorial step
+}
+
+- (void)updateImageViewWithImage:(UIImage *)image
+{
+  self.contentImageView.image = image;
+  self.contentImageHeightConstraint.constant = (image ? kContentImageHeight : 0.0f);
+}
+
+// OBSOLETE, not used
+- (void)updateImageAspectRatioConstraintFromImage:(UIImage *)image
+{
+  AssertTrueOrReturn(image);
+  
+  if (self.imageViewAspectRatioConstraint) {
+    [self.imageView removeConstraint:self.imageViewAspectRatioConstraint];
+  }
+  CGFloat aspectRatio = image.size.height / image.size.width;
+  AssertTrueOrReturn(aspectRatio != 0);
+  
+  NSLayoutConstraint *aspectConstraint = [NSLayoutConstraint constraintWithItem:self.imageView
+                                                                     attribute:NSLayoutAttributeHeight
+                                                                     relatedBy:NSLayoutRelationEqual
+                                                                        toItem:self.imageView
+                                                                     attribute:NSLayoutAttributeWidth
+                                                                    multiplier:aspectRatio
+                                                                      constant:0.f];
+  [self.imageView addConstraint:aspectConstraint];
+  self.imageViewAspectRatioConstraint = aspectConstraint;
 }
 
 @end
