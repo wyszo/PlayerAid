98612610be14468fcf9e672ec7510eaab04be482
Tomasz Wyszomirski <thomas@wyszomirski.co.uk>
2015-03-12 00:44:30 +0100

    Fixed publish tutorial request synchronization implementation - using NSCondition

--- PlayerAid/ServerDataUpdateController.m
+++ PlayerAid/ServerDataUpdateController.m
@@ -9,10 +9,8 @@
 #import "ServerResponseParsing.h"
 
 
-#define InvokeCompletionBlockAndReturn(error) if (completion) { completion(error); return; }
-#define SetBoolToNoAndSignalSemaphore(success,semaphore) { success = NO; dispatch_semaphore_signal(semaphore); }
-#define SignalSemaphore(semaphore) { dispatch_semaphore_signal(semaphore); }
-#define WaitOnSemaphoreAndReturnIfBooleanIsFalse(semaphore,success) { dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER); if (!success) { return; } }
+#define ReturnIfBooleanFalse(boolValue) if (!boolValue) { return; }
+
 
 @implementation ServerDataUpdateController
 
@@ -24,55 +22,62 @@
   // if this is the first time we fetch tutorials - show blocking alert view on failure (and retry)!
 }
 
-
 + (void)saveTutorial:(Tutorial *)tutorial completion:(SaveCompletionBlock)completion
 {
-  dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
+  dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
+    [self privateSaveTutorial:tutorial completion:completion];
+  });
+}
+
++ (void)privateSaveTutorial:(Tutorial *)tutorial completion:(SaveCompletionBlock)completion
+{
+  NSCondition *condition = [[NSCondition alloc] init];
+  [condition lock];
   __block BOOL allRequestsSucceeded = YES;
   
   [[AuthenticatedServerCommunicationController sharedInstance] createTutorial:tutorial completion:^(NSHTTPURLResponse *response, id responseObject, NSError *error) {
     if (!error) {
       NSString *tutorialID = [ServerResponseParsing tutorialIDFromResponseObject:responseObject];
       if (!tutorialID.length) {
-        SetBoolToNoAndSignalSemaphore(allRequestsSucceeded, semaphore);
-        InvokeCompletionBlockAndReturn([NSError incorrectServerResponseError]);
+        [self setBoolReferenceToNo:&allRequestsSucceeded andSignalCondition:condition];
+        return;
       }
       tutorial.serverID = [NSNumber numberWithInteger:[tutorialID integerValue]];
     }
-    else {
-      SignalSemaphore(semaphore);
-    }
+    [self lockSignalAndUnlockCondition:condition];
   }];
   
-  WaitOnSemaphoreAndReturnIfBooleanIsFalse(semaphore, allRequestsSucceeded);
-  
+  [self waitOnCondition:condition andInvokeCompletionBlockWithGenericError:completion ifBooleanValueIsTrue:(!allRequestsSucceeded)];
+  ReturnIfBooleanFalse(allRequestsSucceeded);
+
   [[AuthenticatedServerCommunicationController sharedInstance] submitImageForTutorial:tutorial completion:^(NSHTTPURLResponse *response, id responseObject, NSError *error) {
     if (error) {
-      SetBoolToNoAndSignalSemaphore(allRequestsSucceeded, semaphore);
-      InvokeCompletionBlockAndReturn([NSError genericServerResponseError]);
+      [self setBoolReferenceToNo:&allRequestsSucceeded andSignalCondition:condition];
+      return;
     }
-    else {
-      SignalSemaphore(semaphore);
-    }
+    [self lockSignalAndUnlockCondition:condition];
   }];
 
-  WaitOnSemaphoreAndReturnIfBooleanIsFalse(semaphore, allRequestsSucceeded);
+  [self waitOnCondition:condition andInvokeCompletionBlockWithGenericError:completion ifBooleanValueIsTrue:(!allRequestsSucceeded)];
+  ReturnIfBooleanFalse(allRequestsSucceeded);
   
   [self saveStepsForTutorial:tutorial completion:^(NSError *error) {
     if (error) {
-      SetBoolToNoAndSignalSemaphore(allRequestsSucceeded, semaphore);
-      InvokeCompletionBlockAndReturn([NSError genericServerResponseError]);
+      [self setBoolReferenceToNo:&allRequestsSucceeded andSignalCondition:condition];
+      return;
     }
-    else {
-      SignalSemaphore(semaphore);
-    }
+    [self lockSignalAndUnlockCondition:condition];
   }];
-
-  WaitOnSemaphoreAndReturnIfBooleanIsFalse(semaphore, allRequestsSucceeded);
+  
+  [self waitOnCondition:condition andInvokeCompletionBlockWithGenericError:completion ifBooleanValueIsTrue:(!allRequestsSucceeded)];
+  ReturnIfBooleanFalse(allRequestsSucceeded);
   
   [[AuthenticatedServerCommunicationController sharedInstance] submitTutorialForReview:tutorial completion:^(NSHTTPURLResponse *response, id responseObject, NSError *error) {
     if (error) {
-      InvokeCompletionBlockAndReturn([NSError genericServerResponseError]);
+      if (completion) {
+        completion([NSError genericServerResponseError]);
+        return;
+      }
     }
     else {
       if (completion) {
@@ -80,6 +85,31 @@
       }
     }
   }];
+}
+
++ (void)waitOnCondition:(NSCondition *)condition andInvokeCompletionBlockWithGenericError:(SaveCompletionBlock)completion ifBooleanValueIsTrue:(BOOL)invokeCompletionBlockWithError
+{
+  [condition wait];
+  
+  if (invokeCompletionBlockWithError) {
+    if(completion) {
+      completion([NSError genericServerResponseError]);
+    }
+  }
+}
+
++ (void)setBoolReferenceToNo:(BOOL *)boolValue andSignalCondition:(NSCondition *)condition
+{
+  *boolValue = NO;
+  [condition signal];
+}
+
++ (void)lockSignalAndUnlockCondition:(NSCondition *)condition
+{
+  AssertTrueOrReturn(condition);
+  [condition lock];
+  [condition signal];
+  [condition unlock];
 }
 
 + (void)saveStepsForTutorial:(Tutorial *)tutorial completion:(SaveCompletionBlock)completion
