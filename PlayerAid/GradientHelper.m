d19b86f93f737b54042dad6cb6e4785444bc40c7
Tomasz Wyszomirski <thomas@wyszomirski.co.uk>
2015-03-22 20:26:35 +0100

    Section name background in TutorialTableViewCell now resizes properly to fit section name

--- PlayerAid/GradientHelper.m
+++ PlayerAid/GradientHelper.m
@@ -11,6 +11,8 @@
 {
   CAGradientLayer *gradientLayer = [CAGradientLayer layer];
   gradientLayer.frame = view.bounds;
+  
+  // TODO: move that colour to ColorsHelper
   UIColor *darkBlue = [UIColor colorWithRed:24.0/255.0 green:45.0/255.0 blue:97.0/255.0 alpha:1.0];
   gradientLayer.colors = @[ (id)[[UIColor colorWithWhite:1.0 alpha:0] CGColor], (id)[darkBlue CGColor] ];
   gradientLayer.shouldRasterize = YES;
