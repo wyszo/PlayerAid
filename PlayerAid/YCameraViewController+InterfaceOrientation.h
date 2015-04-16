1594ec45a5dde6a733ad6bd86b4f94773cc217cc
Tomasz Wyszomirski <thomas@wyszomirski.co.uk>
2015-04-16 12:24:59 +0200

    Image step prevented from rotating

---
+++ PlayerAid/YCameraViewController+InterfaceOrientation.h
@@ -0,0 +1,13 @@
+//
+//  YCameraViewController+InterfaceOrientation.h
+//  PlayerAid
+//
+//  Created by Wyszo on 16/04/15.
+//
+//
+
+#import "YCameraViewController.h"
+
+@interface YCameraViewController (InterfaceOrientation)
+
+@end
---
+++ PlayerAid/YCameraViewController+InterfaceOrientation.m
@@ -0,0 +1,13 @@
+//
+//  YCameraViewController+InterfaceOrientation.m
+//  PlayerAid
+//
+//  Created by Wyszo on 16/04/15.
+//
+//
+
+#import "YCameraViewController+InterfaceOrientation.h"
+
+@implementation YCameraViewController (InterfaceOrientation)
+
+@end
