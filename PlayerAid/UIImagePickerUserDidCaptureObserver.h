05f739f0aa9344364da6352e21a38f8376b7f21e
Tomasz Wyszomirski <thomas@wyszomirski.co.uk>
2015-04-16 02:33:55 +0200

    UIImagePickerUserDidCaptureObserver class added

---
+++ PlayerAid/UIImagePickerUserDidCaptureObserver.h
@@ -0,0 +1,13 @@
+//
+//  UIImagePickerUserDidCaptureObserver.h
+//  PlayerAid
+//
+//  Created by Wyszo on 16/04/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UIImagePickerUserDidCaptureObserver : NSObject
+
+@end
---
+++ PlayerAid/UIImagePickerUserDidCaptureObserver.m
@@ -0,0 +1,13 @@
+//
+//  UIImagePickerUserDidCaptureObserver.m
+//  PlayerAid
+//
+//  Created by Wyszo on 16/04/15.
+//
+//
+
+#import "UIImagePickerUserDidCaptureObserver.h"
+
+@implementation UIImagePickerUserDidCaptureObserver
+
+@end
