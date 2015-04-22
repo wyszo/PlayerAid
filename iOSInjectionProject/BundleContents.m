c67db270e053338062714e749eb5d084a47f5121
Tomasz Wyszomirski <thomas@wyszomirski.co.uk>
2015-04-22 10:51:23 +0200

    code injection plugin

--- iOSInjectionProject/BundleContents.m
+++ iOSInjectionProject/BundleContents.m
@@ -3,7 +3,7 @@
 */
 
 #define INJECTION_NOIMPL
-#define INJECTION_BUNDLE InjectionBundle4
+#define INJECTION_BUNDLE InjectionBundle1
 
 #define INJECTION_ENABLED
 #import "/Users/wyszo/Library/Application Support/Developer/Shared/Xcode/Plug-ins/InjectionPlugin.xcplugin/Contents/Resources/BundleInjection.h"
@@ -28,9 +28,9 @@ extern
 };
 #endif
 
-@interface InjectionBundle4 : NSObject
+@interface InjectionBundle1 : NSObject
 @end
-@implementation InjectionBundle4
+@implementation InjectionBundle1
 
 + (void)load {
     Class bundleInjection = NSClassFromString(@"BundleInjection");
@@ -41,7 +41,7 @@ extern
 
 int injectionHook() {
     NSLog( @"injectionHook():" );
-    [InjectionBundle4 load];
+    [InjectionBundle1 load];
     return YES;
 }
 
