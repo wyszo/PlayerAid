8224ee36ef59f4972279b6277db10b750cbcefd5
Tomasz Wyszomirski <thomas@wyszomirski.co.uk>
2015-06-26 01:24:15 +0200

    Switched to Demo environment

--- PlayerAid/Environments.m
+++ PlayerAid/Environments.m
@@ -11,10 +11,13 @@
 // NSString const* kServerBaseURL = @"http://qa.api.playeraid.co.uk/v1/";
 
 // Staging environment
-NSString const* kServerBaseURL = @"http://staging.api.playeraid.co.uk/v1/";
+// NSString const* kServerBaseURL = @"http://staging.api.playeraid.co.uk/v1/";
 
 // Official PlayerAid Mock Server
 //NSString const* kServerBaseURL = @"http://mock.api.playeraid.co.uk/v1/";
 
 // My Internal Mock Server
 // NSString const* kServerBaseURL = @"http://demo6914797.mockable.io/v1/";
+
+// Demo environment
+NSString const* kServerBaseURL = @"http://playeraid-api-demo.azurewebsites.net/v1/";
