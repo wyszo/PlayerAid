09a03ed3ec687e9afefeec554f03927e8a6aed57
Tomasz Wyszomirski <thomas@wyszomirski.co.uk>
2015-06-28 22:48:44 +0200

    Staging environment

--- PlayerAid/Environments.m
+++ PlayerAid/Environments.m
@@ -11,7 +11,7 @@
 // NSString const* kServerBaseURL = @"http://qa.api.playeraid.co.uk/v1/";
 
 // Staging environment
-// NSString const* kServerBaseURL = @"http://staging.api.playeraid.co.uk/v1/";
+NSString const* kServerBaseURL = @"http://staging.api.playeraid.co.uk/v1/";
 
 // Official PlayerAid Mock Server
 //NSString const* kServerBaseURL = @"http://mock.api.playeraid.co.uk/v1/";
@@ -20,4 +20,4 @@
 // NSString const* kServerBaseURL = @"http://demo6914797.mockable.io/v1/";
 
 // Demo environment
-NSString const* kServerBaseURL = @"http://playeraid-api-demo.azurewebsites.net/v1/";
+// NSString const* kServerBaseURL = @"http://playeraid-api-demo.azurewebsites.net/v1/";
