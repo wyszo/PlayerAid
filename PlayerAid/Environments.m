5af43a058e718f3ddc254e5aa3bbbc0b81643569
Tomasz Wyszomirski <thomas@wyszomirski.co.uk>
2015-08-09 20:14:28 +0200

    Revert "env: switched to PRODUCTION"
    This reverts commit 4e124bfe5145c327e4082ac2a341473da8f0f148.

--- PlayerAid/Environments.m
+++ PlayerAid/Environments.m
@@ -5,13 +5,13 @@
 #import "Environments.h"
 
 // Production Server
-NSString const* kServerBaseURL = @"http://api.playeraid.co.uk/v1/";
+//NSString const* kServerBaseURL = @"http://api.playeraid.co.uk/v1/";
 
 // QA Server
 // NSString const* kServerBaseURL = @"http://qa.api.playeraid.co.uk/v1/";
 
 // Staging environment
-//NSString const* kServerBaseURL = @"http://staging.api.playeraid.co.uk/v1/";
+NSString const* kServerBaseURL = @"http://staging.api.playeraid.co.uk/v1/";
 
 // Official PlayerAid Mock Server
 //NSString const* kServerBaseURL = @"http://mock.api.playeraid.co.uk/v1/";
