7e816a47242ba97a520b3a5b4aeddfed0a389426
Tomasz Wyszomirski <thomas@wyszomirski.co.uk>
2015-08-29 22:32:02 +0200

    feat: displaying terms and conditions added

--- PlayerAid/TermsAndConditionsViewController.m
+++ PlayerAid/TermsAndConditionsViewController.m
@@ -3,13 +3,35 @@
 //
 
 #import "TermsAndConditionsViewController.h"
+#import <TWCommonLib/NSString+TWHTMLString.h>
+
+static NSString *const kTermsAndConditionsHTMLFileName = @"T&Cs.html";
+
+
+@interface TermsAndConditionsViewController ()
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+@end
+
 
 @implementation TermsAndConditionsViewController
+
+- (void)viewDidLoad
+{
+  [super viewDidLoad];
+  [self loadWebViewContent];
+}
 
 - (void)viewWillAppear:(BOOL)animated
 {
   [super viewWillAppear:animated];
   [self.navigationController setNavigationBarHidden:NO animated:YES];
+}
+
+- (void)loadWebViewContent
+{
+  NSString *htmlString = [NSString htmlStringFromFileNamed:kTermsAndConditionsHTMLFileName];
+  AssertTrueOrReturn(htmlString.length);
+  [self.webView loadHTMLString:htmlString baseURL:nil];
 }
 
 #pragma mark - Statusbar handling
