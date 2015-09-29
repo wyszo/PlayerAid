ecd9fb73ee6dadd0b2e7464e8532f836132ad51a
Tomasz Wyszomirski <thomas@wyszomirski.co.uk>
2015-09-29 18:49:11 +0200

    fix: profile edit photo action sheet options are not presented if they don’t have action handlers

--- PlayerAid/AlertControllerFactory.m
+++ PlayerAid/AlertControllerFactory.m
@@ -7,24 +7,30 @@
 
 @implementation AlertControllerFactory
 
-+ (UIAlertController *)editProfilePhotoActionControllerFacebookAction:(VoidBlock)facebookAction chooseFromLibraryAction:(VoidBlock)chooseFromLibraryAction takePhotoAction:(VoidBlock)takePhotoAction
++ (UIAlertController *)editProfilePhotoActionControllerFacebookAction:(nullable VoidBlock)facebookAction chooseFromLibraryAction:(nullable VoidBlock)chooseFromLibraryAction takePhotoAction:(nullable VoidBlock)takePhotoAction
 {
   UIAlertController *alertController = [UIAlertController alertControllerWithTitle:nil message:nil preferredStyle:UIAlertControllerStyleActionSheet];
 
-  UIAlertAction *facebookAlertAction = [UIAlertAction actionWithTitle:@"Update from Facebook" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
-    CallBlock(facebookAction);
-  }];
-  [alertController addAction:facebookAlertAction];
+  if (facebookAction) {
+    UIAlertAction *facebookAlertAction = [UIAlertAction actionWithTitle:@"Update from Facebook" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+      CallBlock(facebookAction);
+    }];
+    [alertController addAction:facebookAlertAction];
+  }
 
-  UIAlertAction *libraryAlertAction = [UIAlertAction actionWithTitle:@"Choose from library" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
-    CallBlock(chooseFromLibraryAction);
-  }];
-  [alertController addAction:libraryAlertAction];
-  
-  UIAlertAction *takePhotoAlertAction = [UIAlertAction actionWithTitle:@"Take photo" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
-    CallBlock(takePhotoAction);
-  }];
-  [alertController addAction:takePhotoAlertAction];
+  if (chooseFromLibraryAction) {
+    UIAlertAction *libraryAlertAction = [UIAlertAction actionWithTitle:@"Choose from library" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+      CallBlock(chooseFromLibraryAction);
+    }];
+    [alertController addAction:libraryAlertAction];
+  }
+
+  if (takePhotoAction) {
+    UIAlertAction *takePhotoAlertAction = [UIAlertAction actionWithTitle:@"Take photo" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+      CallBlock(takePhotoAction);
+    }];
+    [alertController addAction:takePhotoAlertAction];
+  }
   
   [alertController addAction:[self cancelAlertAction]];
   return alertController;
