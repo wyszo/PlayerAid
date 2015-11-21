d7de20d14b8e4c08a27d6c73b1ad63a19e4f1f4b
Tomasz Wyszomirski <thomas@wyszomirski.co.uk>
2015-11-21 11:22:38 +0100

    feat: tabBar highlight colour updated to new, brighter PlayerAid blue

--- PlayerAid/ColorsHelper.m
+++ PlayerAid/ColorsHelper.m
@@ -5,10 +5,12 @@
 @import TWCommonLib;
 #import "ColorsHelper.h"
 
-
 #define UIColorMethodMake(name,r,g,b) +(UIColor *)name { return UIColorMake(r,g,b); }
 #define UIColorWithAlphaMethodMake(name,r,g,b,a) +(UIColor *)name { return UIColorWithAlphaMake(r,g,b,a); }
 
+#define UIColorFromHexMethodMake(name,hexColor) +(UIColor *)name { return UIColorFromHex(hexColor); }
+
+static const NSInteger kNewPlayerAidBlue = 0x3f68c5;
 
 @implementation ColorsHelper
 
@@ -18,8 +20,9 @@ UIColorMethodMake(signupTermsAndConditionsPrivacyPolicyTextColor, 133, 134, 149)
 
 UIColorMethodMake(tabBarSelectedTextColor, 42, 70, 136)
 UIColorMethodMake(tabBarUnselectedTextColor, 158, 171, 199)
-UIColorMethodMake(tabBarSelectedImageTintColor, 53, 79, 141)
-UIColorMethodMake(tabBarCreateTutorialBackgroundColor, 53, 79, 141)
+
+UIColorFromHexMethodMake(tabBarSelectedImageTintColor, kNewPlayerAidBlue)
+UIColorFromHexMethodMake(tabBarCreateTutorialBackgroundColor, kNewPlayerAidBlue)
 UIColorMethodMake(tabBarCreateTutorialTextColor, 255, 255, 255)
 
 UIColorMethodMake(navigationBarColor, 42, 70, 136)
@@ -32,12 +35,12 @@ UIColorMethodMake(tutorialsUnselectedFilterButtonColor, 26, 43, 80)
 UIColorMethodMake(tutorialsUnselectedFilterButtonTextColor, 91, 103, 129)
 UIColorWithAlphaMethodMake(tutorialGradientBlueColor, 24, 45, 97, 0.8)
 
-UIColorMethodMake(tutorialCommentsBarBackgroundColor, 60, 100, 198)
+UIColorFromHexMethodMake(tutorialCommentsBarBackgroundColor, kNewPlayerAidBlue)
 UIColorMethodMake(commentsTimeAgoLabelColor, 43, 71, 139);
 UIColorMethodMake(makeCommentPostButtonActiveBackgroundColor, 91, 127, 214)
 UIColorMethodMake(makeCommentPostButtonInactiveBackgroundColor, 159, 179, 226)
 
-UIColorMethodMake(playerAidBlueColor, 44, 72, 134)
+UIColorMethodMake(playerAidBlueColor, 44, 72, 134) // old, darker one
 UIColorMethodMake(editProfileViewBackgroundColor, 245, 245, 245)
 UIColorMethodMake(editProfileSubviewsBorderColor, 200, 199, 204)
 UIColorMethodMake(editProfileFacebookButtonColor, 81, 101, 157)
