409ed202d35d2338c1a00440fc8f1019c6567dd9
Tomasz Wyszomirski <thomas@wyszomirski.co.uk>
2015-11-22 23:46:41 +0100

    feat: comment ‘more’ button visible only when a comment has more than 5 lines

--- PlayerAid/TutorialCommentCell.m
+++ PlayerAid/TutorialCommentCell.m
@@ -29,7 +29,7 @@ static const NSInteger kMaxFoldedCommentNumberOfLines = 5;
   [self.avatarImageView makeCircular];
   [self tw_configureForFullWidthSeparators];
   self.timeAgoLabel.textColor = [ColorsHelper commentsTimeAgoLabelColor];
-  [self setupCommentLabel];
+  [self setupCommentTextLabel];
 }
 
 - (void)prepareForReuse
@@ -39,20 +39,9 @@ static const NSInteger kMaxFoldedCommentNumberOfLines = 5;
   [self.avatarImageView cancelImageRequestOperation];
 }
 
-- (void)setupCommentLabel
+- (void)setupCommentTextLabel
 {
   self.commentLabel.numberOfLines = kMaxFoldedCommentNumberOfLines;
-}
-
-#pragma mark - IBActions
-
-- (IBAction)moreButtonPressed:(id)sender {
-  BOOL bothBeforeAndAfterAnimationBlocksSet = (self.willChangeCellHeightBlock && self.didChangeCellHeightBlock);
-  AssertTrueOrReturn(bothBeforeAndAfterAnimationBlocksSet && "either none of the blocks or both have to be set (willChange.. should call beginUpdates on tableView and didChange should call endUpdates");
-  
-  CallBlock(self.willChangeCellHeightBlock);
-  self.commentLabel.numberOfLines = 0; // this will trigger animations if willChange/didChange blocks contain calls to beginUpdates and endUpdates on tableView
-  CallBlock(self.didChangeCellHeightBlock);
 }
 
 #pragma mark TWConfigurableFromDictionary
@@ -65,8 +54,28 @@ static const NSInteger kMaxFoldedCommentNumberOfLines = 5;
   self.authorLabel.text = commentAuthor.name;
   self.commentLabel.text = comment.text;
   self.timeAgoLabel.text = [comment.createdOn shortTimeAgoSinceNow];
+  [self updateMoreButtonVisibility];
   
   [commentAuthor placeAvatarInImageView:self.avatarImageView];
+}
+
+#pragma mark - private
+
+- (void)updateMoreButtonVisibility
+{
+  self.moreButton.hidden = ([self.commentLabel tw_lineCount] <= kMaxFoldedCommentNumberOfLines);
+}
+
+#pragma mark - IBActions
+
+- (IBAction)moreButtonPressed:(id)sender {
+  BOOL bothBeforeAndAfterAnimationBlocksSet = (self.willChangeCellHeightBlock && self.didChangeCellHeightBlock);
+  AssertTrueOrReturn(bothBeforeAndAfterAnimationBlocksSet && "either none of the blocks or both have to be set (willChange.. should call beginUpdates on tableView and didChange should call endUpdates");
+  
+  CallBlock(self.willChangeCellHeightBlock);
+  self.commentLabel.numberOfLines = 0; // this will trigger animations if willChange/didChange blocks contain calls to beginUpdates and endUpdates on tableView
+  self.moreButton.hidden = YES; // cell extended, we don't need it anymore
+  CallBlock(self.didChangeCellHeightBlock);
 }
 
 @end
