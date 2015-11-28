5af890a39896b7037aa34141f5e6e2d011030815
Tomasz Wyszomirski <thomas@wyszomirski.co.uk>
2015-11-28 11:25:35 +0100

    feat: edit comment input view changes

--- PlayerAid/EditCommentInputViewController.m
+++ PlayerAid/EditCommentInputViewController.m
@@ -4,12 +4,24 @@
 
 #import "EditCommentInputViewController.h"
 
+static NSString *const kNibName = @"EditCommentInputView";
+
 @interface EditCommentInputViewController ()
 @property (weak, nonatomic) IBOutlet UITextView *inputTextView;
 
 @end
 
 @implementation EditCommentInputViewController
+
+#pragma mark - Init
+
+- (instancetype)init
+{
+  self = [super initWithNibName:kNibName bundle:nil];
+  if (self) {
+  }
+  return self;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
