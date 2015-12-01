0692f44c3d2519cfd2d34fa2045b36304e215460
Tomasz Wyszomirski <thomas@wyszomirski.co.uk>
2015-12-01 18:50:13 +0100

    fix: text step max character count updated to 500 (PM-533)

--- PlayerAid/GlobalSettings.m
+++ PlayerAid/GlobalSettings.m
@@ -5,5 +5,5 @@
 @import CoreGraphics;
 #import "GlobalSettings.h"
 
-const NSInteger kMaxTextStepCharactersCount = 300;
+const NSInteger kMaxTextStepCharactersCount = 500;
 const CGFloat kJPEGCompressionQuality = 0.85;
