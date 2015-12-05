57530dcf53f528d833be9365aeab74f7a4c2b976
Tomasz Wyszomirski <thomas@wyszomirski.co.uk>
2015-12-05 08:02:39 +0100

    feat: embedding CommentBottomBarView in the TableViewCell

--- PlayerAid/CommentBottomBarView.swift
+++ PlayerAid/CommentBottomBarView.swift
@@ -1,9 +1,35 @@
-//
-//  CommentBottomBarView.swift
 //  PlayerAid
-//
-//  Created by Wyszo on 05/12/15.
-//
-//
 
-import Foundation
+import UIKit
+import TWCommonLib
+
+class CommentBottomBarView: UIView {
+  
+  private var view: UIView!
+  
+  // MARK: Init
+  
+  override init(frame: CGRect) {
+    super.init(frame:frame);
+    setupView();
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder);
+    setupView();
+  }
+  
+  private func setupView() {
+    self.view = self.tw_loadView(self.view, fromNibNamed:"CommentBottomBar");
+  }
+  
+  // MARK: IBActions
+  
+  // Those two methods should cetrainly be part of the view controller, not the view, right?
+  
+  @IBAction func likeButtonPressed(sender: AnyObject) {
+  }
+  
+  @IBAction func numberOfLikesButtonPressed(sender: AnyObject) {
+  }
+}
